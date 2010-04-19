11934da1e88fc7ca68bf8e4e94e71434236f0e9d
Masaki KOMAGATA <komagata@gmail.com>
2010-04-19 16:35:21 +0900

    デプロイ時に設定ファイルにシンボリックリンクを貼るように修正。

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,15 +4,22 @@ default_run_options[:pty] = true
 ssh_options[:forward_agent] = true
 set :application, 'help-me-hackers'
 set :scm, :git
-set :repository,  "git@github.com:komagata/help-me-hackers.git"
+set :repository,  "git://github.com/komagata/help-me-hackers.git"
 set :use_sudo, false
 set :user, 'deployer'
 
 namespace :deploy do
   desc "Restarting mod_rails with restart.txt"
-  task :restart, :roles => :app, :except => { :no_release => true } do
+  task :restart, :roles => :app, :except => {:no_release => true} do
     run "touch #{current_path}/tmp/restart.txt"
   end
+
+  desc "make symlink to config file"
+  task :symlink_config, :roles => :app, :except => {:no_release => true} do
+    run "ln -s #{deploy_to}/shared/database.yml #{current_path}/config/database.yml"
+    run "ln -s #{deploy_to}/shared/twitter_auth.yml #{current_path}/config/twitter_auth.yml"
+  end
+  after 'deploy:symlink', 'deploy:symlink_config'
 end
 
 after :deploy, 'deploy:cleanup'
