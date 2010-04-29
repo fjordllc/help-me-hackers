bf93b3153522af3c711ed85bab105e3b563c7ce6
Masaki KOMAGATA <komagata@gmail.com>
2010-04-30 04:39:15 +0900

    救出前に戻す

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -7,7 +7,7 @@ class VotesController < ApplicationController
     vote.user = current_user
 
     if vote.save
-      tweet(Time.new.sec.to_s + vote.comment)
+      tweet(vote.comment)
       count = Vote.count(:conditions => [
         'voteable_id = ? and voteable_type = ?',
         vote.voteable_id, vote.voteable_type])
