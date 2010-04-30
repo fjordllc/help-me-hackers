b5f9da93cc643650df5ea78f3942d241b82b65f1
Masaki KOMAGATA <komagata@gmail.com>
2010-05-01 00:46:55 +0900

    fixed

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -10,10 +10,10 @@ config.action_controller.perform_caching             = true
 config.action_view.cache_template_loading            = true
 
 # See everything in the log (default is :info)
-# config.log_level = :debug
+config.log_level = :debug
 
 # Use a different logger for distributed setups
-# config.logger = SyslogLogger.new
+config.logger = SyslogLogger.new
 
 # Use a different cache store in production
 # config.cache_store = :mem_cache_store
