3f5e9008f25aa3b5fc6b30a4057b124072244f57
Masaki KOMAGATA <komagata@gmail.com>
2010-04-30 15:51:06 +0900

    added

---
+++ app/models/problem.rb
@@ -0,0 +1,39 @@
+class Problem < ActiveRecord::Base
+  acts_as_taggable
+
+  belongs_to :user
+  belongs_to :language
+  belongs_to :license
+  belongs_to :category
+  has_many :hacks
+  has_many :votes, :as => :voteable, :dependent => :destroy
+
+  validates_presence_of :title,
+                        :description,
+                        :category,
+                        :language,
+                        :license,
+                        :user,
+                        :bounty
+  validates_length_of :title, :within => 4..255
+  validates_length_of :description, :minimum => 30
+
+  def self.increment_view_by_id(id)
+    problem = self.find(id)
+    problem.view += 1
+    problem.save
+  end
+
+  def solved?
+    !!correct_hack
+  end
+
+  def correct_hack
+    Hack.find(:first,
+      :conditions => ['problem_id = ? AND correct = TRUE', id])
+  end
+
+  def solver
+    correct_hack.user
+  end
+end
