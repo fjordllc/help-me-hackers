d11a5a690cf43cf0c1fdfe930ff94bbe2f035a93
Masaki KOMAGATA <komagata@gmail.com>
2010-05-01 13:28:13 +0900

    exception_notificationを外してhoptoad追加。

--- config/deploy.rb
+++ config/deploy.rb
@@ -23,3 +23,10 @@ namespace :deploy do
 end
 
 after :deploy, 'deploy:cleanup'
+
+
+Dir[File.join(File.dirname(__FILE__), '..', 'vendor', 'gems', 'hoptoad_notifier-*')].each do |vendored_notifier|
+  $: << File.join(vendored_notifier, 'lib')
+end
+
+require 'hoptoad_notifier/capistrano'
---
+++ config/initializers/hoptoad.rb
@@ -0,0 +1,3 @@
+HoptoadNotifier.configure do |config|
+  config.api_key = '3361c65a64bb5f92eb4cfd2343191073'
+end
