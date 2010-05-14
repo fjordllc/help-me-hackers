88bdd4f89ca3a9cb97c441956ead3edc5e01583d
Masaki KOMAGATA <komagata@gmail.com>
2010-05-14 21:25:57 +0900

    リリースTweetを追加

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,6 +7,7 @@ set :scm, :git
 set :repository,  "git://github.com/komagata/help-me-hackers.git"
 set :use_sudo, false
 set :user, 'deployer'
+set :version, Time.new.strftime('%Y%m%d%H%M%S')
 
 namespace :deploy do
   desc "Restarting mod_rails with restart.txt"
@@ -14,13 +15,45 @@ namespace :deploy do
     run "touch #{current_path}/tmp/restart.txt"
   end
 
-  desc "make symlink to config file"
+  desc "Create symlink to config file"
   task :symlink_config, :roles => :app, :except => {:no_release => true} do
     run "ln -s #{deploy_to}/shared/database.yml #{release_path}/config/database.yml"
     run "ln -s #{deploy_to}/shared/twitter_auth.yml #{release_path}/config/twitter_auth.yml"
     run "ln -s #{deploy_to}/shared/newrelic.yml #{release_path}/config/newrelic.yml"
   end
   after 'deploy:finalize_update', 'deploy:symlink_config'
+
+  desc "Create release tag"
+  task :tagging, :roles => :app, :except => {:no_release => true} do
+    run "echo '#{version}' > #{release_path}/VERSION"
+    run_locally "echo '#{version}' > VERSION"
+    run_locally "git tag v#{version}"
+  end
+
+  namespace :notify do
+    desc "Tweet release"
+    task :tweet do
+      require 'rubygems'
+      require 'twitter'
+      require 'pit'
+      
+      config = Pit.get('twitter', :require => {
+        'consumer_token_key' => '',
+        'consumer_secret'    => '',
+        'access_token_key'   => '',
+        'access_secret'      => ''
+      })
+      
+      oauth = Twitter::OAuth.new(config['consumer_token_key'], config['consumer_secret'])
+      oauth.authorize_from_access(config['access_token_key'], config['access_secret'])
+      
+      client = Twitter::Base.new(oauth)
+
+      msg = "Version #{version}をリリースしました。 http://help-me-hackers #hmh"
+      client.update(msg)
+      puts msg
+    end
+  end
 end
 
 after :deploy, 'deploy:cleanup'
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,3 +2,6 @@ set :deploy_to, "/var/www/#{application}"
 role :app, "help-me-hackers.com"
 role :web, "help-me-hackers.com"
 role :db,  "help-me-hackers.com", :primary => true
+
+after :deploy, 'deploy:tagging'
+after 'deploy:tagging', 'deploy:notify:tweet'
