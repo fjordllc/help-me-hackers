af1c6610c03e1016d9b90cb0f392d3b2dc29d4a3
Masaki KOMAGATA <komagata@gmail.com>
2010-05-25 19:00:07 +0900

    １４０文字制限追加

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -2,4 +2,5 @@ class Vote < ActiveRecord::Base
   belongs_to :voteable, :polymorphic => true
   belongs_to :user
   validates_uniqueness_of :voteable_id, :scope => [:voteable_type, :user_id]
+  validates_length_of :comment, :maximum => 140
 end
