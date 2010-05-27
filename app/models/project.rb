c960eb87f6e8a902585cf6f515f1f9d299b0cbd5
Masaki KOMAGATA <komagata@gmail.com>
2010-05-27 16:42:16 +0900

    * プロジェクトのdescriptionを必須にした。 * プロジェクトのリポジトリパスとWebページURLが無い場合は非表示にした。

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,6 +3,7 @@ class Project < ActiveRecord::Base
   has_many :tasks
   validates_presence_of :name
   validates_uniqueness_of :name
+  validates_presence_of :description
 
   def solved_task_count
     task_count(true)
