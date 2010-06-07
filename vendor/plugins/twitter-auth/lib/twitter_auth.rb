2d73e5ab027dc358618c5aad10e0dc194ffdc175
Masaki KOMAGATA <komagata@gmail.com>
2010-06-07 16:03:04 +0900

    twitter_auth.ymlをERBで読むように修正。

--- vendor/plugins/twitter-auth/lib/twitter_auth.rb
+++ vendor/plugins/twitter-auth/lib/twitter_auth.rb
@@ -3,7 +3,7 @@ module TwitterAuth
 
   def self.config(environment=RAILS_ENV)
     @config ||= {}
-    @config[environment] ||= YAML.load(File.open(RAILS_ROOT + '/config/twitter_auth.yml').read)[environment]
+    @config[environment] ||= YAML.load(ERB.new(File.open(RAILS_ROOT + '/config/twitter_auth.yml').read))[environment]
   end
 
   def self.base_url
