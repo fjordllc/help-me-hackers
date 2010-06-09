d8121c63a47d0a1668b16d8120748b500f528e87
Masaki KOMAGATA <komagata@gmail.com>
2010-06-09 21:46:59 +0900

    一時的にブラウザで言語判断するのを辞める

--- config/initializers/locale.rb
+++ config/initializers/locale.rb
@@ -1,2 +1,2 @@
-I18n.load_path += Dir[File.join(RAILS_ROOT, 'lib', 'locale', '*.{rb,yml}')]
-I18n.supported_locales = Dir[File.join(RAILS_ROOT, 'lib', 'locale', '*.{rb,yml}')].collect{|v| File.basename(v, ".*")}.uniq
+#I18n.load_path += Dir[File.join(RAILS_ROOT, 'lib', 'locale', '*.{rb,yml}')]
+#I18n.supported_locales = Dir[File.join(RAILS_ROOT, 'lib', 'locale', '*.{rb,yml}')].collect{|v| File.basename(v, ".*")}.uniq
