d714f56f4d4fe72fc56db9d32c5ad7e04e61c3e4
Masaki Komagata <komagata@gmail.com>
2014-10-27 19:11:05 +0900

    Upgrade rails 2.3.17

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -2,9 +2,10 @@ class TopController < ApplicationController
   TASKS_PER_PAGE = 20
 
   def index
-    @tasks = Task.paginate(
-      :page     => params[:page],
-      :per_page => TASKS_PER_PAGE,
-      :order    => 'id DESC')
+    @tasks = Task.all
+#    @tasks = Task.paginate(
+#      :page     => params[:page],
+#      :per_page => TASKS_PER_PAGE,
+#      :order    => 'id DESC')
   end
 end
--- config/environment.rb
+++ config/environment.rb
@@ -1,10 +1,32 @@
 # Be sure to restart your server when you modify this file
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.8' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.17' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
+
+# monkey patch for 2.0. Will ignore vendor gems.
+if RUBY_VERSION >= "2.0.0"
+  module Gem
+    def self.source_index
+      sources
+    end
+
+    def self.cache
+      sources
+    end
+
+    SourceIndex = Specification
+
+    class SourceList
+      # If you want vendor gems, this is where to start writing code.
+      def search( *args ); []; end
+      def each( &block ); end
+      include Enumerable
+    end
+  end
+end
 
 Rails::Initializer.run do |config|
   # Settings in config/environments/* take precedence over those specified here.
@@ -19,6 +41,9 @@ Rails::Initializer.run do |config|
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
+  config.gem 'iconv' if RUBY_VERSION >= "2.0.0"
+  config.gem 'pacecar'
+  config.gem 'will_paginate', version: '~> 2.3.16'
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
